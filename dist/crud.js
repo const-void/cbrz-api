425e9d11bd5609e2ecd0a36d131afffb5ee1f200
const-void <constvoid@eml.cc>
2020-06-20 08:17:38 -0400

    remove .gitignored files

--- dist/crud.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var express = require('express');
-const app = express.Router();
-/*
-app.get("/", (req,res) => {
-    res.json({"message":"ok"});
-});*/
-module.exports = app;
-//# sourceMappingURL=crud.js.map
