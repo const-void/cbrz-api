c61e0b338d0654f2df5057b5fce1953c6edb6ee0
const-void <constvoid@eml.cc>
2020-11-14 13:04:54 -0500

    allow for binary responses

--- src/routes/app-cors.ts
+++ src/routes/app-cors.ts
@@ -1,11 +1,11 @@
 import cors, { CorsOptions } from "cors";
 
-const options:CorsOptions = {
-    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
-    credentials: true,
-    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-    origin: "http://localhost:4200",
-    preflightContinue: false
-  };
+const options: CorsOptions = {
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token", 'Response-Type'],
+  credentials: true,
+  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  origin: "http://localhost:4200",
+  preflightContinue: false
+};
 
-export var appCors=cors(options);
+export var appCors = cors(options);
